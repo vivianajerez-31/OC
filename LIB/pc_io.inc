21a59cde8f1838b3b450175dfc3ab28d28ae1fc5
vivianajerez-31 <viviana.jerez.guerrero@uabc.edu.mx>
2026-09-08 22:41:56 +0000

    agregado p4 y librerias

---
+++ LIB/pc_io.inc
@@ -0,0 +1,9 @@
+;********** procedimientos de pc_linux.o *******************************
+	extern clrscr			
+	extern gotoxy
+	extern puts
+	extern putchar
+	extern getch
+	extern getche
+
+;********************* fin *********************************************
--- P4/p4.asm
+++ P4/p4.asm
@@ -8,10 +8,15 @@ _start:
 	mov edx, msg		; edx = dirección de la cadena msg
 	call puts			; imprime cadena msg terminada en valor nulo (0)
 
+	mov edx, msg2		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
+
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
 section	.data
 msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+msg2	db  'hola2',10,0 
+
 
 
